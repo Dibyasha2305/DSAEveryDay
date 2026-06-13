febb7b33632fb9534d9b1dbc8c079200f25b1201
Dibyasha <dibyashamishra2021@gmail.com>
2026-06-13 10:23:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int search(vector<int>& arr, int x) {
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
