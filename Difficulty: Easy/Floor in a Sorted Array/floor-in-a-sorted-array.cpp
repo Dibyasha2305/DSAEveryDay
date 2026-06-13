e97c153e08c2a96434995338be8960aa881ad98e
Dibyasha <dibyashamishra2021@gmail.com>
2026-06-13 12:27:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, int x) {
+        int low = 0;
+        int high = arr.size() - 1;
+        int ans = -1; // Default to -1 if no floor exists
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] <= x) {
+                ans = mid;     
+                low = mid + 1; 
+            } 
+            else {
+                high = mid - 1; 
+            }
+        }
+        
+        return ans;
+    }
+};
