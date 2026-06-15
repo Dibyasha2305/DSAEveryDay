7329e9b3ae9314d29c2f51013805c5c27b35e1fb
Dibyasha <dibyashamishra2021@gmail.com>
2026-06-15 09:29:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Single Number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int getSingle(vector<int>& arr) {
+        int result = 0;
+        
+        for (int num : arr) {
+            result ^= num;
+        }
+        
+        return result;
+    }
+};
