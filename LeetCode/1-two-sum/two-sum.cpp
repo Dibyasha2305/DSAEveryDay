e27e8a04e17e936ca76c68550b04ef560ef2582c
Dibyasha <dibyashamishra2021@gmail.com>
2026-07-26 10:50:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (21.17%) - LeetSync

--- LeetCode/1-two-sum/two-sum.cpp
+++ LeetCode/1-two-sum/two-sum.cpp
@@ -6,24 +6,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        unordered_map<int, int> map; 
-        
-       
+        unordered_map<int, int> seen;
+
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            
-            
-            if (map.find(complement) != map.end()) {
-                
-                return {map[complement], i}; 
+            int comp = target - nums[i];
+
+            if (seen.find(comp) != seen.end()) {
+                return {seen[comp], i};
             }
-            
-            
-            map[nums[i]] = i; 
+
+            seen[nums[i]] = i;
         }
-        
-        
-        return {}; 
+
+        return {};
     }
 };
