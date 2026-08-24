c88e7a73f55ac97e9d3ef81caa031546aa0611d8
Dibyasha <dibyashamishra2021@gmail.com>
2026-08-24 22:26:04 +0530

    Time: 15 ms (69.43%) | Memory: 25 MB (71.31%) - LeetSync

--- LeetCode/49-group-anagrams/group-anagrams.cpp
+++ LeetCode/49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,22 @@
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> map;
-        
-        for (string s : strs) {
-            string key = s;                
-            sort(key.begin(), key.end());   
-            
-            map[key].push_back(s);           
+        unordered_map<string, vector<string>> mp;
+
+        for (string word : strs) {
+            string key = word;
+
+            sort(key.begin(), key.end());
+
+            mp[key].push_back(word);
         }
-        
-        vector<vector<string>> result;
-        for (auto& pair : map) {
-            result.push_back(pair.second);   
+
+        vector<vector<string>> ans;
+
+        for (auto& pair : mp) {
+            ans.push_back(pair.second);
         }
-        
-        return result;
+
+        return ans;
     }
 };
