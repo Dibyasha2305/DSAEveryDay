d3a7224850e3214a11f92d3b9edafaf289ee1dfa
Dibyasha <dibyashamishra2021@gmail.com>
2026-09-03 12:10:10 +0530

    Time: 45 ms (70.09%) | Memory: 29 MB (92.44%) - LeetSync

---
+++ LeetCode/15-3sum/3sum.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            
+            // Skip duplicate fixed values
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+
+                int value = nums[i] + nums[left] + nums[right];
+
+                if (value == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+
+                    // Skip duplicate left values
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+
+                    // Skip duplicate right values
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++;
+                    right--;
+                }
+                else if (value < 0) {
+                    left++;
+                }       
+                else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
